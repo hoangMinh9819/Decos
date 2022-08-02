92d31260ec03c587778a0775ad0fae3bd0edbb39
LeSang2003 <91366410+LeSang2003@users.noreply.github.com>
2022-08-02 14:19:14 +0700

    thanh sang

--- resources/views/khach_hang/ho_so_khach_hang.blade.php
+++ resources/views/khach_hang/ho_so_khach_hang.blade.php
@@ -30,6 +30,36 @@
                                         class="form-control" id="email" placeholder="Email" required style="width: 700px;">
                                 </div>
                                 <div class="form-group">
+                                <label for="matkhau">Mật khẩu</label>
+                                <input type="password" value="{{ $khach_hang->MAT_KHAU }}" name="mat_khau"
+                                    style="width: 700px;" class="input form-control" id="matkhau" placeholder="matkhau"
+                                    required="true" aria-label="password" aria-describedby="basic-addon1">
+                                <div class="input-group-append">
+                                    <span class="input-group-text" onclick="password_show_hide();">
+                                        <i class="fas fa-eye" id="show_eye"></i>
+                                        <i class="fas fa-eye-slash d-none" id="hide_eye"></i>
+                                    </span>
+                                </div>
+
+                                <script>
+                                    function password_show_hide() {
+                                        var x = document.getElementById("matkhau");
+                                        var show_eye = document.getElementById("show_eye");
+                                        var hide_eye = document.getElementById("hide_eye");
+                                        hide_eye.classList.remove("d-none");
+                                        if (x.type === "matkhau") {
+                                            x.type = "text";
+                                            show_eye.style.display = "none";
+                                            hide_eye.style.display = "block";
+                                        } else {
+                                            x.type = "matkhau";
+                                            show_eye.style.display = "block";
+                                            hide_eye.style.display = "none";
+                                        }
+                                    }
+                                </script>
+                            </div>
+                                <div class="form-group">
                                     <label for="dienthoai">Điện Thoại</label>
                                     <input type="text" value="{{ $khach_hang->DIEN_THOAI }}" name="dien_thoai"
                                         class="form-control" id="dienthoai" placeholder="Điện Thoại" style="width: 700px;">
