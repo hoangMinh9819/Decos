3f5656c9d369977876a7a7a9a87b6dfc2a965de3
Minh Anh <93037946+Candynox@users.noreply.github.com>
2022-08-03 12:17:49 +0700

    minh anh

---
+++ resources/views/khach_hang/bao_mat_cookie.blade.php
@@ -0,0 +1,139 @@
+@extends('khach_hang.bo_cuc_khach_hang')
+@section('noi_dung')
+    <div class="container ">
+        <div>
+            <h2 class="title text-center">Chính sách bảo mật và cookie</h2>
+            <div class="container p-3 my-3 border" style="width: 1100px">
+                <h3 style="color: #FE980F">1. NHỮNG GÌ ĐƯỢC ĐIỀU CHỈNH BỞI CHÍNH SÁCH BẢO MẬT NÀY VÀ CÁC CHÍNH SÁCH KHÁC CÓ
+                    ÁP DỤNG CHO BẠN KHÔNG?
+                </h3>
+                <p> Chính sách bảo mật này bao gồm các tình huống mà chúng tôi, Decos, truy cập, thu thập, lưu trữ xử lý, sử
+                    dụng, tiết lộ hoặc chuyển thông tin cá nhân của bạn dưới dạng giấy hoặc điện tử. Nó cũng bao gồm các
+                    tình
+                    huống trong đó các đối tác được chấp thuận hoặc các thành viên khác của Kering sử dụng thông tin cá nhân
+                    của
+                    bạn thay mặt chúng tôi.
+                </p>
+                <p>Trong một số trường hợp, các chính sách khác sẽ áp dụng cho bạn. Nó có thể là trường hợp khi bạn sử dụng
+                    một số sản phẩm hoặc dịch vụ nhất định và trong trường hợp này: <br>
+                    - Bạn sẽ được thông báo về các chính sách khác này trong khi sử dụng các sản phẩm hoặc dịch vụ mà chúng
+                    áp dụng. <br>
+                    - Các chính sách như vậy sẽ thay thế chính sách bảo mật này trừ khi họ đề cập rõ ràng đến nó. <br>
+                    Chính sách bảo mật này không bao gồm các sản phẩm hoặc dịch vụ bạn có thể sử dụng như được cung cấp bởi
+                    các công ty khác hoạt động trên tài khoản của chính họ.</p>
+                <p>Chính sách bảo mật của chúng tôi không mở rộng cho các dịch vụ được cung cấp bởi các công ty khác hoạt
+                    động trên tài khoản của riêng họ, chẳng hạn như người được nhượng quyền và người được cấp phép hoặc khi
+                    bạn chia sẻ thông tin thông qua phương tiện truyền thông xã hội (WeChat, WhatsApp, Baidu, Kakao Talk,
+                    Facebook, Twitter, Instagram, v.v.) hoặc các nền tảng trực tuyến khác do các công ty khác sở hữu và quản
+                    lý. Các công ty khác này có chính sách bảo mật của riêng họ, vì vậy hãy nhớ rằng cách họ sử dụng bất kỳ
+                    thông tin cá nhân nào bạn cung cấp cho họ sẽ phải tuân theo các quy tắc của họ chứ không phải của chúng
+                    tôi.</p>
+                <h3 style="color: #FE980F">2. AI THU THẬP & SỬ DỤNG THÔNG TIN CÁ NHÂN CỦA BẠN?</h3>
+                <p>Để có thể cung cấp các sản phẩm và dịch vụ của chúng tôi cho bạn, chúng tôi cần chia sẻ thông tin cá nhân
+                    của bạn với các đối tác được phê duyệt hoặc các thành viên khác của Kering, những người thường đóng vai
+                    trò là "bên xử lý dữ liệu" của chúng tôi. Tất cả đều bị ràng buộc bởi nghĩa vụ thực hiện các biện pháp
+                    bảo mật thích hợp để bảo vệ dữ liệu cá nhân thuộc quyền sở hữu của họ và bị ràng buộc bởi một thỏa thuận
+                    bảo mật nghiêm ngặt và các điều khoản hợp đồng cụ thể về "cách thức" và "thời điểm" họ được phép sử dụng
+                    thông tin cá nhân của bạn thay mặt chúng tôi. Xin lưu ý rằng một số đối tác được phê duyệt nhất định,
+                    chẳng hạn như bên nhận quyền và người được cấp phép hoặc phương tiện truyền thông xã hội của chúng tôi,
+                    cũng có thể xác định "lý do" và "cách thức" thông tin cá nhân của bạn được sử dụng. Họ có chính sách
+                    quyền riêng tư và cookie của riêng họ, vì vậy hãy nhớ rằng thông tin cá nhân bạn cung cấp cho họ sẽ phải
+                    tuân theo các quy tắc của họ chứ không phải của chúng tôi.</p>
+                <p>Luật pháp địa phương của bạn có thể không đề cập đến các thuật ngữ "người kiểm soát dữ liệu" hoặc "bộ xử
+                    lý dữ liệu", mà tương ứng với các thuật ngữ như "người dùng dữ liệu", "chủ sở hữu", "doanh nghiệp", "bên
+                    chịu trách nhiệm", v.v. và là "trung gian dữ liệu", "nhà điều hành", v.v. Để hiểu vai trò của chúng tôi,
+                    xin lưu ý rằng chúng tôi, Decod, là tổ chức quyết định các phương pháp và mục đích xử lý thông tin cá
+                    nhân của bạn bằng cách xác định "tại sao" và "cách thức" thông tin cá nhân của bạn được thu thập và sử
+                    dụng. </p>
+                <h3 style="color: #FE980F">3. TẠI SAO CHÚNG TÔI THU THẬP THÔNG TIN CÁ NHÂN CỦA BẠN?</h3>
+                <p>Decos sẽ thu thập, lưu giữ, sử dụng và tiết lộ thông tin cá nhân của bạn cho các mục đích được mô tả
+                    trong phần này. Việc xử lý thông tin cá nhân của bạn sẽ tuân thủ luật về quyền riêng tư của bạn và dựa
+                    trên các cơ sở pháp lý được nêu trong phần này.
+
+                    Nếu bạn muốn biết thêm thông tin về mục đích và cơ sở pháp lý, bạn có thể liên hệ với chúng tôi theo số
+                    privacy@decos.com (hoặc xem Bạn có thể liên hệ với chúng tôi như thế nào?). </p>
+                <p>Chúng tôi sẽ sử dụng thông tin cá nhân của bạn để quản lý mối quan hệ của chúng tôi với bạn và để thực
+                    hiện các nghĩa vụ của chúng tôi theo hợp đồng mà bạn ký kết với chúng tôi khi bạn mua hoặc yêu cầu sản
+                    phẩm hoặc dịch vụ từ chúng tôi. Điều này có thể bao gồm các hoạt động khác nhau như kế toán nội bộ, xử
+                    lý thanh toán hoặc giao hàng của bạn.</p>
+                <p>Một số thông tin cụ thể nhất định có thể được yêu cầu để cung cấp cho bạn thẻ quà tặng, dịch vụ tùy chỉnh
+                    hoặc khi bạn đặt trước sản phẩm, yêu cầu sửa chữa sản phẩm hoặc mua sản phẩm hoặc dịch vụ qua điện thoại
+                    hoặc trên trang web của chúng tôi hoặc thị trường. Trong những trường hợp như vậy, chúng tôi có thể truy
+                    cập, thu thập, lưu trữ và sử dụng thông tin cá nhân của bạn để quản lý giao hàng, kho và trả hàng. </p>
+                <table border="3" style="width: 850px;  margin-left: auto;
+                margin-right: auto;">
+                    <tr style="text-indent: 3px">
+                        <th>Các mục đích mà chúng tôi xử lý thông tin cá nhân của bạn như sau:</th>
+                        <th>Việc xử lý thông tin cá nhân của bạn được chứng minh trên cơ sở pháp lý sau:</th>
+                    </tr>
+                    <tr style="text-indent: 3px">
+                        <th>Cung cấp cho bạn các sản phẩm và dịch vụ bạn đã mua hoặc yêu cầu theo cách khác tại các cửa hàng
+                            của chúng tôi hoặc trực tuyến. <br>
+                            Điều này liên quan đến bất kỳ hành động nào cần thiết để thực hiện nghĩa vụ của chúng tôi theo
+                            hợp đồng mua bán hoặc dịch vụ mà bạn ký kết với chúng tôi (xử lý thanh toán, giao hàng, quản lý
+                            trả lại, v.v.)</th>
+                        <th>Thực hiện hợp đồng</th>
+                    </tr>
+                </table> <br>
+                <p>Khi bạn đã yêu cầu chúng tôi tạo hồ sơ Decos của bạn, thông tin cá nhân của bạn sẽ được liên kết với hồ
+                    sơ này, nơi chúng tôi sẽ giữ tất cả thông tin chúng tôi có về bạn. Điều này có thể bao gồm thông tin cá
+                    nhân mà chúng tôi có được trực tiếp từ bạn cũng như từ các nguồn khác theo luật hiện hành. Thông tin
+                    trong hồ sơ Decos của bạn rất quan trọng để cung cấp cho bạn trải nghiệm được cá nhân hóa và phù hợp với
+                    Decod, và do đó nó cần được cập nhật để có trải nghiệm tốt nhất.</p>
+                <p> Khi bạn đã yêu cầu chúng tôi tạo hồ sơ Decos của bạn, chúng tôi sẽ bổ sung cho nó thông tin cá nhân như
+                    mua hàng, sở thích và chi tiết liên hệ của bạn. Trong trường hợp này, xin lưu ý rằng chúng tôi sử dụng
+                    dữ liệu nhận dạng cá nhân, chẳng hạn như địa chỉ email hoặc số điện thoại của bạn, để liên kết một tương
+                    tác hoặc mua hàng với hồ sơ Decos của bạn. Điều này cũng áp dụng khi thanh toán với tư cách là khách
+                    trên trang web của chúng tôi khi mua hàng.</p>
+                <p>Để giữ cho thông tin cá nhân của bạn chính xác, chúng tôi sẽ thực hiện mọi bước hợp lý để đảm bảo rằng
+                    thông tin cá nhân mà chúng tôi nắm giữ được cập nhật đúng cách. Theo luật hiện hành về quyền riêng tư
+                    của bạn, bạn có thể có quyền chỉnh sửa thông tin của mình bất kỳ lúc nào và trong trường hợp vui lòng
+                    cho chúng tôi biết nếu bất kỳ thông tin nào của bạn cần cập nhật hoặc chỉnh sửa và chúng tôi sẽ nỗ lực
+                    hết sức để phát hiện và chỉnh sửa thông tin cá nhân không chính xác (làm sạch dữ liệu) và xóa bất kỳ tập
+                    dữ liệu trùng lặp nào. </p>
+                <table border="3" style="width: 850px;  margin-left: auto;
+                margin-right: auto;">
+                    <tr style="text-indent: 3px">
+                        <th>Các mục đích mà chúng tôi xử lý thông tin cá nhân của bạn như sau:</th>
+                        <th>Việc xử lý thông tin cá nhân của bạn được chứng minh trên cơ sở pháp lý sau:</th>
+                    </tr>
+                    <tr style="text-indent: 3px">
+                        <th>Quản lý, quản lý và duy trì hồ sơ Decos của bạn sau khi bạn đã yêu cầu chúng tôi tạo hồ sơ đó.
+                            Điều này có thể bao gồm làm sạch dữ liệu và loại bỏ trùng lặp.</th>
+                        <th>Thực hiện hợp đồng hoặc lợi ích hợp pháp của chúng tôi (nếu có) Khi hồ sơ của bạn đã được tạo
+                            theo yêu cầu của bạn, để bổ sung cho nó thông tin cá nhân mà bạn
+                            cung cấp cho chúng tôi trong các tương tác của chúng tôi (và lợi ích hợp pháp đó không bị chi
+                            phối bởi lợi ích hoặc quyền cơ bản của chính bạn)</th>
+                    </tr>
+                </table> <br>
+                <p>Với mục đích cung cấp cho bạn trải nghiệm Decos, chúng tôi sẽ thu thập, lưu trữ, xử lý, sử dụng, tiết lộ
+                    và chuyển thông tin cá nhân của bạn để có thể nhận dạng bạn. Điều này sẽ cho phép chúng tôi cung cấp cho
+                    bạn sự hỗ trợ phù hợp mà bạn đang yêu cầu hoặc chia sẻ thông tin liên quan với bạn. Ngoài ra, khi bạn
+                    liên hệ với chúng tôi, chúng tôi có thể giữ lại thông tin cá nhân mà bạn chia sẻ với chúng tôi để luôn
+                    cung cấp cho bạn trải nghiệm tùy chỉnh và cá nhân hóa như vậy.</p>
+                <h3 style="color: #FE980F">4. CHÚNG TÔI THU THẬP THÔNG TIN CÁ NHÂN NÀO?</h3>
+                <p>Với mục đích cung cấp cho bạn trải nghiệm Decos, chúng tôi sẽ thu thập, lưu trữ, xử lý, sử dụng, tiết lộ
+                    và chuyển thông tin cá nhân của bạn để có thể nhận dạng bạn. Điều này sẽ cho phép chúng tôi cung cấp cho
+                    bạn sự hỗ trợ phù hợp mà bạn đang yêu cầu hoặc chia sẻ thông tin liên quan với bạn. Ngoài ra, khi bạn
+                    liên hệ với chúng tôi, chúng tôi có thể giữ lại thông tin cá nhân mà bạn chia sẻ với chúng tôi để luôn
+                    cung cấp cho bạn trải nghiệm tùy chỉnh và cá nhân hóa như vậy.</p>
+                <p> Thông tin phi tiếp thị có thể bao gồm, nhưng không giới hạn ở: gửi email cho bạn để xác minh danh tính
+                    của bạn khi bạn đăng ký dịch vụ của chúng tôi; gửi email cho bạn để nhập mật khẩu và / hoặc đặt lại tên
+                    người dùng; thông báo cho bạn rằng một dịch vụ cụ thể đã hoặc sẽ bị đình chỉ để bảo trì hoặc chấm dứt;
+                    thông báo cho bạn về việc nâng cấp, khắc phục hoặc sự cố ảnh hưởng đến các dịch vụ của chúng tôi có thể
+                    liên quan đến thông tin cá nhân của bạn; thông báo cho bạn về bản cập nhật cho chính sách bảo mật hoặc
+                    Điều khoản sử dụng của chúng tôi;</p>
+                <p>Danh sách dưới đây được cung cấp để cung cấp cho bạn cái nhìn tổng quan về loại thông tin chúng tôi có
+                    thể thu thập. Tuy nhiên, việc thu thập thông tin này không có hệ thống: chỉ những thông tin cần thiết để
+                    thực hiện từng mục đích mới được thu thập và sử dụng.</p>
+                <p>nó có thể bao gồm sở thích của bạn về các sự kiện thời trang, cách bạn đã sử dụng một số sản phẩm nhất
+                    định, dị ứng thực phẩm khi bạn chọn chia sẻ thông tin này với chúng tôi một cách rõ ràng, thông tin cá
+                    nhân liên quan đến gia đình bạn (nếu bạn đã có được sự đồng ý của họ để chia sẻ thông tin đó với chúng
+                    tôi, khi điều này được yêu cầu bởi luật hiện hành), thói quen cuộc sống của bạn, cách bạn sử dụng trang
+                    web của chúng tôi hoặc các trang web của bên thứ ba như phương tiện truyền thông xã hội, câu trả lời của
+                    bạn cho các cuộc khảo sát của chúng tôi </p>
+            </div>
+
+        </div>
+    </div>
+@endsection
