cff693bcb5d2471158113ce0edddb0550befc19d
hoangMinh9819 <76242408+hoangMinh9819@users.noreply.github.com>
2022-08-03 13:44:11 +0700

    hoangminh

--- resources/views/nhan_vien/quan_li_the_loai/search_danh_muc.blade.php
+++ resources/views/nhan_vien/quan_li_the_loai/search_danh_muc.blade.php
@@ -15,13 +15,13 @@
             ?>
             <div class="row w3-res-tb">
                 <div class="col-sm-3 m-b-xs">
-                    <select class="input-sm form-control w-sm inline v-middle">
+                    <!-- <select class="input-sm form-control w-sm inline v-middle">
                         <option value="0">Bulk action</option>
                         <option value="1">Delete selected</option>
                         <option value="2">Bulk edit</option>
                         <option value="3">Export</option>
                     </select>
-                    <button class="btn btn-sm btn-default">Apply</button>
+                    <button class="btn btn-sm btn-default">Apply</button> -->
                 </div>
                 <div class="col-sm-4">
                 </div>
@@ -71,17 +71,17 @@
             <footer class="panel-footer">
                 <div class="row">
                     <div class="col-sm-5 text-center">
-                        <small class="text-muted inline m-t-sm m-b-sm">showing 20-30 of 50 items</small>
+                        <!-- <small class="text-muted inline m-t-sm m-b-sm">showing 20-30 of 50 items</small> -->
                     </div>
                     <div class="col-sm-7 text-right text-center-xs">
-                        <ul class="pagination pagination-sm m-t-none m-b-none">
+                        <!-- <ul class="pagination pagination-sm m-t-none m-b-none">
                             <li><a href=""><i class="fa fa-chevron-left"></i></a></li>
                             <li><a href="">1</a></li>
                             <li><a href="">2</a></li>
                             <li><a href="">3</a></li>
                             <li><a href="">4</a></li>
                             <li><a href=""><i class="fa fa-chevron-right"></i></a></li>
-                        </ul>
+                        </ul> -->
                     </div>
                 </div>
             </footer>
