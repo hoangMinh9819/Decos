fa72b2179c3cfff32f01364705c125a82ac5cc14
Quoc Thai <93020112+QuocThaiDang@users.noreply.github.com>
2022-08-07 16:01:01 +0700

    Thai update

--- resources/views/khach_hang/ho_so_khach_hang.blade.php
+++ resources/views/khach_hang/ho_so_khach_hang.blade.php
@@ -5,35 +5,33 @@
             <section class="panel">
                 <h2 class="title text-center">Hồ sơ khách hàng</h2>
                 <div class="panel-body">
-                        <div class="container">
-                            <form role="form"
-                                action="{{ URL::to('/cap_nhat_ho_so_khach_hang/' . $khach_hang->ID_NGUOI_DUNG) }}"
-                                method="post">
-                                {{ csrf_field() }}
-                                <div class="form-group">
-                                    <label for="hinhanh">Chân Dung</label><br>
-                                    <img src="{{ URL::to('/uploads/nguoi_dung/' . $khach_hang->HINH_ANH) }}" width="300">
-                                </div>
-                                <div class="form-group">
-                                    <label for="ten">Tên khách hàng</label>
-                                    <input type="text" value="{{ $khach_hang->HO_TEN }}" name="ten"
-                                        class="form-control" id="ten" placeholder="Tên danh mục" style="width: 700px;">
-                                </div>
-                                <div class="form-group">
-                                    <label for="diachi">Địa Chỉ</label>
-                                    <input type="text" value="{{ $khach_hang->DIA_CHI }}" name="dia_chi"
-                                        class="form-control" id="diachi" placeholder="Địa chỉ" style="width: 700px;">
-                                </div>
-                                <div class="form-group">
-                                    <label for="email">Email</label>
-                                    <input type="text" value="{{ $khach_hang->EMAIL }}" name="email"
-                                        class="form-control" id="email" placeholder="Email" required style="width: 700px;">
-                                </div>
-                                <div class="form-group">
-                                <label for="matkhau">Mật khẩu</label>
+                    <div class="container">
+                        <form role="form" action="" method="get">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <label for="hinhanh">Chân Dung</label><br>
+                                <img src="{{ URL::to('/uploads/nguoi_dung/' . $khach_hang->HINH_ANH) }}" width="300">
+                            </div>
+                            <div class="form-group">
+                                <label for="ten">Tên khách hàng</label> <br>
+                                <input type="text" value="{{ $khach_hang->HO_TEN }}" name="ten" class="form-control"
+                                    id="ten" placeholder="Tên danh mục" style="width: 700px;" readonly>
+                            </div>
+                            <div class="form-group">
+                                <label for="diachi">Địa Chỉ</label> <br>
+                                <input type="text" value="{{ $khach_hang->DIA_CHI }}" name="dia_chi" class="form-control"
+                                    id="diachi" placeholder="Địa chỉ" style="width: 700px;" readonly>
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email</label> <br>
+                                <input type="text" value="{{ $khach_hang->EMAIL }}" name="email" class="form-control"
+                                    id="email" placeholder="Email" required style="width: 700px;" readonly>
+                            </div>
+                            <div class="form-group">
+                                <label for="matkhau">Mật khẩu</label> <br>
                                 <input type="password" value="{{ $khach_hang->MAT_KHAU }}" name="mat_khau"
                                     style="width: 700px;" class="input form-control" id="matkhau" placeholder="matkhau"
-                                    required="true" aria-label="password" aria-describedby="basic-addon1">
+                                    required="true" aria-label="password" aria-describedby="basic-addon1" readonly>
                                 <div class="input-group-append">
                                     <span class="input-group-text" onclick="password_show_hide();">
                                         <i class="fas fa-eye" id="show_eye"></i>
@@ -59,18 +57,68 @@
                                     }
                                 </script>
                             </div>
-                                <div class="form-group">
-                                    <label for="dienthoai">Điện Thoại</label>
-                                    <input type="text" value="{{ $khach_hang->DIEN_THOAI }}" name="dien_thoai"
-                                        class="form-control" id="dienthoai" placeholder="Điện Thoại" style="width: 700px;">
-                                </div>
-                                <button type="submit" name="ho_so_khach_hang" class="btn btn-info">Cập Nhật Thông
-                                    Tin</button>
-                            </form>
-                        </div>
+                            <div class="form-group">
+                                <label for="dienthoai">Điện Thoại</label> <br>
+                                <input type="text" value="{{ $khach_hang->DIEN_THOAI }}" name="dien_thoai"
+                                    class="form-control" id="dienthoai" placeholder="Điện Thoại" style="width: 700px;"
+                                    readonly>
+                            </div>
+                            <button class="btn btn-primary"> <a
+                                    href="{{ URL::to('/sua_ho_so_khach_hang/' . $khach_hang->ID_NGUOI_DUNG) }}"
+                                    class="active" ui-toggle-class="">
+                                    SỬA
+                                    <!--<i class="fa fa-pencil-square-o text-success text-active"></i>-->
+                                </a></button>
+                        </form>
+                    </div>
                 </div>
             </section>
 
         </div>
     </div>
 @endsection
+<style>
+    input[type=text],
+    select {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }
+
+    input[type=email],
+    select {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }
+
+    input[type=date],
+    select {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }
+
+    /* input[type=file], select {
+      width: 100%;
+      padding: 12px 20px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      box-sizing: border-box;
+    } */
+</style>
+
