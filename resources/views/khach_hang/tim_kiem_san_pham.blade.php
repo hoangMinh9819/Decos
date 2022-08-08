4ef902d29e614d98ac6b0299b6876d214537af16
hoangMinh9819 <76242408+hoangMinh9819@users.noreply.github.com>
2022-08-08 15:48:11 +0700

    m

--- resources/views/khach_hang/tim_kiem_san_pham.blade.php
+++ resources/views/khach_hang/tim_kiem_san_pham.blade.php
@@ -27,23 +27,15 @@
                     <img src={{asset('uploads/san_pham/'.$gia_tri->HINH_ANH)}} alt="" />
                         <h2>{{number_format($gia_tri->GIA)}} VND</h2>
                         <p>{{$gia_tri->TEN_SP}}</p>
-                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm giỏ hàng</a>
                     </div>
                     <div class="product-overlay">
                                 <a href="{{URL::to('/chi_tiet_san_pham/'.$gia_tri->ID_SAN_PHAM)}}">
                         <div class="overlay-content"><img src={{asset('uploads/san_pham/'.$gia_tri->HINH_ANH_HAI)}} alt="" width="290px" /></a>
                             <h2>{{number_format($gia_tri->GIA)}} VND</h2>
                             <p>{{$gia_tri->TEN_SP}}</p>
-                            <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm giỏ hàng</a>
                         </div>
                     </div>
                 </div>
-                <!-- <div class="choose">
-                    <ul class="nav nav-pills nav-justified">
-                        <li><a href=""><i class="fa fa-plus-square"></i>Add to wishlist</a></li>
-                        <li><a href=""><i class="fa fa-plus-square"></i>Add to compare</a></li>
-                    </ul>
-                </div> -->
             </div>
         </div>
         @endforeach
