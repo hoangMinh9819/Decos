c1b80f23013d5cf763acc2c689dccdb7ad3b2a56
hoangMinh9819 <76242408+hoangMinh9819@users.noreply.github.com>
2022-08-09 09:52:07 +0700

    m

--- resources/views/nhan_vien/quan_li_ho_so/ho_so_nhan_vien.blade.php
+++ resources/views/nhan_vien/quan_li_ho_so/ho_so_nhan_vien.blade.php
@@ -11,10 +11,6 @@
                     <form role="form" action="{{URL::to('/cap_nhat_danh_muc/')}}" method="post">
                         {{csrf_field()}}
                         <div class="form-group">
-                            <label for="1">Chân Dung</label><br>
-                            <img src="{{URL::to('/uploads/nguoi_dung/'.$nhan_vien->HINH_ANH)}}" width="300">
-                        </div>
-                        <div class="form-group">
                             <label for="1">Họ Tên</label>
                             <input type="text" value="{{$nhan_vien->HO_TEN}}" name="ten_danh_muc" class="form-control" id="1">
                         </div>
